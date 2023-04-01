51e90e849b215d05b8b5c7f40f899bb0f5cdd439
User <User@WINDOWS-2R2MKCU>
2023-04-01 23:28:03 +0400

    Checking all the user's datas with methods.

--- Classwork 2/Classwork 2/Program.cs
+++ Classwork 2/Classwork 2/Program.cs
@@ -1,144 +1,356 @@
-﻿namespace TestCode
+﻿using System.Xml.Linq;
+
+namespace TestCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string[] currencies = new string[] { "USD", "TRY", "EUR", "RUB", "CNY", "GEL", "UAH" };
-            double[] currenciesPrices = new double[] { 1.7000, 0.0898, 1.8011, 0.0224, 0.2441, 0.6561, 0.0460 };
-            int currencyCount = currencies.Length;
-            int whileNumber = currencies.Length;
-            string alpha3;
-            int number = 0;
-            int idx = 0;
-            int newInt = currencies.Length;
 
-
-
-            Console.Write("1. ");
-            Console.WriteLine("Show recent currency rates");
-
-            Console.Write("2. ");
-            Console.WriteLine("Find curreny by code");
-
-            Console.Write("3. ");
-            Console.WriteLine("Calculate amount by currency");
-
-            Console.Write("4. ");
-            Console.WriteLine("Exit");
-
-            Console.WriteLine("--------------------------------------------------------");
-
-            Console.Write("Which option do you need : ");
-            string option = Console.ReadLine();
-
-            while (true)
+            //Checking user's name is correct or not
+            Console.Write("Name : ");
+            string name = Console.ReadLine()!;
+            bool check = CheckName(name);
+            do
             {
-
-                if (option == "Show recent currency rates" || option == "1")
+                if (check)
                 {
-
-                    while (idx < currencyCount)
-                    {
-                        Console.WriteLine("AZN to " + currencies[idx] + " = " + currenciesPrices[idx]);
-                        idx++;
-                    }
-
-                    idx = 0;
-
-                    Console.WriteLine();
-                    Console.WriteLine("--------------------------------------------------------");
-                    Console.WriteLine();
-
-                    Console.Write("Which option do you need : ");
-                    option = Console.ReadLine();
+                    Console.WriteLine("Please write your name correctly!");
+                    Console.Write("Name : ");
+                    name = Console.ReadLine()!;
+                    check = CheckName(name);
                 }
-                else if (option == "2" || option == "Find curreny by code")
+            } while (check);
+
+            Console.WriteLine();
+
+            //Checking user's surname is correct or not
+            Console.Write("Surname :");
+            string surname = Console.ReadLine()!;
+            check = CheckSurname(surname);
+            do
+            {
+                if (check)
                 {
-                    idx = 0;
-                    number = 0;
-                    alpha3 = currencies[idx];
-                    whileNumber = currencies.Length;
-
-                    Console.Write("Please write you alpha3 code : ");
-                    alpha3 = Console.ReadLine();
-
-
-                    if (alpha3 != currencies[idx])
-                    {
-                        Console.WriteLine("This currency is not available");
-                    }
-
-                    while (currencies.Length > idx)
-                    {
-                        if (alpha3 == currencies[idx])
-                        {
-                            idx = number;
-
-                            Console.WriteLine("AZN to " + currencies[idx] + " = " + currenciesPrices[idx]);
-                            Console.WriteLine();
-                            Console.WriteLine("----------------------------------------------------------------------------");
-                            Console.WriteLine();
-                            break;
-                        }
-                        else
-                        {
-                            idx++;
-                            number++;
-                        }
-                    }
-
-                    Console.Write("Which option do you need : ");
-                    option = Console.ReadLine();
-
+                    Console.WriteLine("Please write your surname correctly!");
+                    Console.Write("Surname : ");
+                    surname = Console.ReadLine()!;
+                    check = CheckSurname(surname);
                 }
-                else if (option == "3" || option == "Calculate amount by currency")
+            } while (check);
+
+            Console.WriteLine();
+
+            //Checking user's father's name is correct or not
+            Console.Write("Father's name : ");
+            string fathersName = Console.ReadLine()!;
+            check = CheckFathersName(fathersName);
+            do
+            {
+                if (check)
                 {
-
-                    idx = 0;
-                    number = 0;
-                    whileNumber = currencies.Length;
-
-                    Console.Write("Please, first, write your amount : ");
-
-                    double amountOfAZN = double.Parse(Console.ReadLine());
-
-
-                    Console.Write("Now, write which currency do yo need : ");
-
-                    alpha3 = Console.ReadLine();
-
-
-
-                    while (currencies.Length > idx)
-                    {
-                        if (alpha3 == currencies[idx])
-                        {
-                            idx = number;
-
-                            Console.WriteLine("AZN to " + currencies[idx] + " = " + amountOfAZN / currenciesPrices[idx]);
-                            Console.WriteLine();
-                            Console.WriteLine("----------------------------------------------------------------------------");
-                            Console.WriteLine();
-                            break;
-                        }
-                        else
-                        {
-                            idx++;
-                            number++;
-                        }
-                    }
-                    Console.Write("Which option do you need : ");
-                    option = Console.ReadLine();
+                    Console.WriteLine("Please write your Father's name correctly!");
+                    Console.Write("Father's name : ");
+                    surname = Console.ReadLine()!;
+                    check = CheckFathersName(fathersName);
                 }
-                else
+            } while (check);
+
+            Console.WriteLine();
+
+            //Checking user's age is correct or not
+            Console.Write("Age : ");
+            int age = 0;
+            try
+            {
+                age = int.Parse(Console.ReadLine()!);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Write your age correctly!");
+                throw;
+            }
+
+            check = CheckAge(age);
+            do
+            {
+                if (check)
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("Program stopped working.");
-                    Console.WriteLine();
+                    Console.WriteLine("Please write your age correctly!");
+                    Console.Write("Age :");
+                    age = int.Parse(Console.ReadLine()!);
+                    check = CheckAge(age);
+                }
+
+            } while (check);
+            Console.WriteLine();
+
+            //Checking user's FIN code is correct or not
+            Console.Write("FIN code :");
+            string FINcode = Console.ReadLine()!;
+            check = CheckFINcode(FINcode);
+            do
+            {
+                if (check)
+                {
+                    Console.WriteLine("Please write your FIN code correctly!");
+                    Console.Write("FIN code : ");
+                    FINcode = Console.ReadLine()!;
+                    check = CheckFINcode(FINcode);
+                }
+            } while (check);
+            Console.WriteLine();
+
+            //Checking user's phone number is correct or not
+            Console.Write("Phone number : ");
+            string phoneNumber = Console.ReadLine()!;
+            check = CheckPhoneNumber(phoneNumber);
+            do
+            {
+                if (check)
+                {
+                    Console.WriteLine("Please write your phone number correctly! ( Start with '+994' )");
+                    Console.Write("Phone number : ");
+                    phoneNumber = Console.ReadLine()!;
+                    check = CheckPhoneNumber(phoneNumber);
+                }
+            } while (check);
+            Console.WriteLine();
+
+            //Checking user's position
+            Console.Write("Position : ");
+            string position = Console.ReadLine()!;
+            check = CheckPosition(position);
+            do
+            {
+                if (check)
+                {
+                    Console.WriteLine("Sorry, your position is not suitable for our requirements");
+                    Console.Write("Position : ");
+                    position = Console.ReadLine()!;
+                    check = CheckPosition(position);
+                }
+
+            } while (check);
+            Console.WriteLine();
+
+            //Checking user's income
+            Console.Write("Income : ");
+            int income = int.Parse(Console.ReadLine()!);
+            check = CheckIncome(income);
+            do
+            {
+                if (check)
+                {
+                    Console.WriteLine("Sorry, your income must be between 1500 and 5000");
+                    Console.WriteLine("Income : ");
+                    position = Console.ReadLine()!;
+                    check = CheckIncome(income);
+                }
+            } while (check);
+
+
+            //If all datas are correct user will see that
+            Console.WriteLine($"{name} {surname} {fathersName}, Age : {age}, Phone number : {phoneNumber}, Position {position}");
+        }
+        static bool CheckName(string name)
+        {
+            bool check = false;
+
+            char[] uppercaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                for (int j = 0; j < name.Length; j++)
+                {
+                    if (name[i] == numbers[j])
+                    {
+                        check = true; break;
+                    }
+                }
+                if (check)
+                {
                     break;
                 }
             }
+
+            if (2 < name.Length && name.Length < 20)
+            {
+                for (int i = 0; i < uppercaseLetters.Length; i++)
+                {
+                    if (name[0] == uppercaseLetters[i])
+                    {
+                        return check;
+                    }
+                    else
+                    {
+                        return !check;
+                    }
+                }
+            }
+            else
+            {
+                return !check;
+            }
+            return check;
+        }
+        static bool CheckSurname(string surname)
+        {
+            bool check = false;
+
+            char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] uppercaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            for (int i = 0; i < surname.Length; i++)
+            {
+                for (int j = 0; j < surname.Length; j++)
+                {
+                    if (surname[i] == numbers[j])
+                    {
+                        check = true; break;
+                    }
+                }
+                if (check)
+                {
+                    break;
+                }
+            }
+
+            if (2 < surname.Length && surname.Length < 20)
+            {
+                for (int i = 0; i < uppercaseLetters.Length; i++)
+                {
+                    if (surname[0] == uppercaseLetters[i])
+                    {
+                        return check;
+                    }
+                    else
+                    {
+                        return !check;
+                    }
+                }
+            }
+            else
+            {
+                return !check;
+            }
+            return check;
+        }
+        static bool CheckFathersName(string fathersName)
+        {
+            bool check = false;
+            char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] uppercaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            for (int i = 0; i < fathersName.Length; i++)
+            {
+                for (int j = 0; j < fathersName.Length; j++)
+                {
+                    if (fathersName[i] == numbers[j])
+                    {
+                        check = true; break;
+                    }
+                }
+                if (check)
+                {
+                    break;
+                }
+            }
+
+            for (int i = 0; i < fathersName.Length; i++)
+            {
+                for (int j = 0; j < fathersName.Length; j++)
+                {
+                    if (fathersName[i] == numbers[j])
+                    {
+                        check = true; break;
+                    }
+                }
+                if (check)
+                {
+                    break;
+                }
+            }
+
+            if (2 < fathersName.Length && fathersName.Length < 20)
+            {
+                for (int i = 0; i < uppercaseLetters.Length; i++)
+                {
+                    if (fathersName[0] == uppercaseLetters[i])
+                    {
+                        return check;
+                    }
+                    else
+                    {
+                        return !check;
+                    }
+                }
+            }
+            else
+            {
+                return !check;
+            }
+            return check;
+        }
+        static bool CheckAge(int age)
+        {
+            if (age <= 18 || age >= 65)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static bool CheckFINcode(string FINcode)
+        {
+            bool check = true;
+            char[] uppercaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            if (FINcode.Length == 7)
+            {
+                for (int i = 0; i < uppercaseLetters.Length; i++)
+                {
+                    if (FINcode[0] == uppercaseLetters[i])
+                    {
+                        check = false;
+                        return check;
+                    }
+                }
+            }
+            return check;
+        }
+        static bool CheckPhoneNumber(string phoneNumber)
+        {
+            if (phoneNumber[0] == '+' && phoneNumber[1] == '9' && phoneNumber[2] == '9' && phoneNumber[3] == '4')
+            {
+                if (phoneNumber.Length == 13)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static bool CheckPosition(string position)
+        {
+            if (position == "HR" || position == "Audit" || position == "Engineer")
+            {
+                return false;
+            }
+            return true;
+        }
+        static bool CheckIncome(int money)
+        {
+            if (money < 1500 || money > 5000)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
---
+++ String[]/String[]/Program.cs
@@ -0,0 +1,169 @@
+﻿namespace String__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 1
+
+            //Console.Write("Please write your name : ");
+            //string name = Console.ReadLine()!;
+            //Console.WriteLine();
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    char letters = name[i];
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //task 2
+
+            //char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+
+            //Console.Write("Please write your name : ");
+            //string name = Console.ReadLine()!;
+
+            //Console.WriteLine();
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    char letters = name[i];
+            //}
+
+            //bool check = false;
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    for (int a = 0; a < name.Length; a++)
+            //    {
+            //        if (name[i] == numbers[a])
+            //        {
+            //            check = true;
+            //            break;
+            //        }
+            //    }
+            //    if (check)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //if (check)
+            //{
+            //    Console.WriteLine("Error");
+            //}
+            //else if (!check)
+            //{
+            //    for (int i = 0; i < name.Length; i++)
+            //    {
+            //        Console.WriteLine(name[i]);
+            //    }
+            //}
+
+            //task 3
+
+            //char[] uppercaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+            //                            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            //char[] lowerLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            //                        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            //Console.Write("Please write your name : ");
+            //string name = Console.ReadLine()!;
+
+            //Console.WriteLine();
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    char letters = name[i];
+            //}
+
+            //bool check = false;
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    for (int a = 0; a < name.Length; a++)
+            //    {
+            //        if (name[i] == uppercaseLetters[a] || name[i] == lowerLetters[a])
+            //        {
+            //            check = true;
+            //            break;
+            //        }
+            //    }
+            //    if (check)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //if (check)
+            //{
+            //    Console.WriteLine("Error");
+            //}
+            //else if (!check)
+            //{
+            //    for (int i = 0; i < name.Length; i++)
+            //    {
+            //        Console.WriteLine(name[i]);
+            //    }
+            //}
+
+            //task 4
+
+            //Console.Write("Pls write something : ");
+            //string name = Console.ReadLine();
+            //int Length = name.Length;
+
+            //for (int i = 0; i < Length; i++)
+            //{
+            //    char letters = name[i];
+            //}
+
+            //for (int i = 0; i < Length; Length--)
+            //{
+            //    Console.WriteLine(name[Length - 1]);
+            //}
+
+            //task 5
+
+            //Console.Write("Pls write sentences : ");
+            //string name = Console.ReadLine()!;
+            //string x = "";
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    x += name[i];
+            //    if (name[i] == ' ' || i == name.Length - 1)
+            //    {
+            //        Console.WriteLine(x);
+            //        x = null;
+            //    }
+            //}
+
+            //task 6
+
+            //Console.WriteLine("Pls write your number : ");
+            //string number = Console.ReadLine()!;
+
+            //for (int i = number.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(number[i]);
+            //}
+
+            //task 7
+
+            Console.WriteLine("Pls write your number : ");
+            string number = Console.ReadLine()!;
+            string word = "";
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                word += number[i];
+                Console.Write(word);
+                if (false)//davami yazilmalidi!!!
+                {
+
+                }
+            }
+        }
+    }
+}
