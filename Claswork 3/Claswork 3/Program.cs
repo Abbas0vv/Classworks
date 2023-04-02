bad6761cff3462c88b81138d28d85f1d90109e72
User <User@WINDOWS-2R2MKCU>
2023-04-02 21:41:12 +0400

    Checking user's datas with methods

---
+++ Claswork 3/Claswork 3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Claswork_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
